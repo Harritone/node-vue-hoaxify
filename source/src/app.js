01bebba40ef1405f3c3d5ac56c75de34bfd91e6a
Hariton <yourrasik@gmail.com>
2021-09-22 14:56:26 +1000

    RFC: refactor

--- source/src/app.js
+++ source/src/app.js
@@ -1,18 +1,8 @@
 const express = require('express');
 const app = express();
-const User = require('./user/User');
-const bcrypt = require('bcrypt');
+const UserRouter = require('./user/userRouter');
 
 app.use(express.json());
-
-app.post('/api/v1/users', (req, res) => {
-  bcrypt.hash(req.body.password, 10).then((hash) => {
-    const user = { ...req.body, password: hash };
-    // const user = Object.assign({}, req.body, { password: hash });
-    User.create(user).then(() => {
-      return res.send({ message: 'User created' });
-    });
-  });
-});
+app.use(UserRouter);
 
 module.exports = app;
---
+++ source/src/user/userRouter.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const UserService = require('./UserService');
+const router = express.Router();
+
+router.post('/api/v1/users', async (req, res) => {
+  await UserService.save(req.body);
+  return res.send({ message: 'User created' });
+});
+
+module.exports = router;
