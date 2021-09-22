c56727ebd8d9cb7481be77ed0e3ec8f3da0d9f82
Hariton <yourrasik@gmail.com>
2021-09-22 14:27:57 +1000

    Add: implement sign_up endpoint

--- source/__test__/UserRegister.spec.js
+++ source/__test__/UserRegister.spec.js
@@ -1,5 +1,15 @@
 const request = require('supertest');
 const app = require('../src/app');
+const User = require('../src/user/User');
+const sequelize = require('../src/config/database');
+
+beforeAll(() => {
+  return sequelize.sync();
+});
+
+beforeEach(() => {
+  return User.destroy({ truncate: true });
+});
 
 describe('User Registration', () => {
   it('returns 200 OK when signup is valid', (done) => {
@@ -29,6 +39,59 @@ describe('User Registration', () => {
         expect(response.body.message).toBe('User created');
         done();
       });
-    // .expect(200, done);
+  });
+
+  it('saves user to database', (done) => {
+    request(app)
+      .post('/api/v1/users')
+      .send({
+        username: 'user1',
+        email: 'user1@mail.com',
+        password: 'P4ssword',
+      })
+      .then(() => {
+        // query user table
+        User.findAll().then((userList) => {
+          expect(userList.length).toBe(1);
+          done();
+        });
+      });
+  });
+
+  it('saves the username and email to database', (done) => {
+    request(app)
+      .post('/api/v1/users')
+      .send({
+        username: 'user1',
+        email: 'user1@mail.com',
+        password: 'P4ssword',
+      })
+      .then(() => {
+        // query user table
+        User.findAll().then((userList) => {
+          const savedUser = userList[0];
+          expect(savedUser.username).toBe('user1');
+          expect(savedUser.email).toBe('user1@mail.com');
+          done();
+        });
+      });
+  });
+
+  it('hashes the password in database', (done) => {
+    request(app)
+      .post('/api/v1/users')
+      .send({
+        username: 'user1',
+        email: 'user1@mail.com',
+        password: 'P4ssword',
+      })
+      .then(() => {
+        // query user table
+        User.findAll().then((userList) => {
+          const savedUser = userList[0];
+          expect(savedUser.password).not.toBe('P4ssword');
+          done();
+        });
+      });
   });
 });
--- source/src/app.js
+++ source/src/app.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const app = express();
+const User = require('./user/User');
+const bcrypt = require('bcrypt');
+
+app.use(express.json());
 
 app.post('/api/v1/users', (req, res) => {
-  return res.send({ message: 'User created' });
+  bcrypt.hash(req.body.password, 10).then((hash) => {
+    const user = { ...req.body, password: hash };
+    // const user = Object.assign({}, req.body, { password: hash });
+    User.create(user).then(() => {
+      return res.send({ message: 'User created' });
+    });
+  });
 });
 
 module.exports = app;
