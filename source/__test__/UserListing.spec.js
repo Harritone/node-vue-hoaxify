c2faddcf5bc56ce9695029d2b4f9600e7b1c8a59
Hariton <yourrasik@gmail.com>
2021-10-06 15:00:01 +1000

    Add: page content

--- source/__test__/UserListing.spec.js
+++ source/__test__/UserListing.spec.js
@@ -1,5 +1,29 @@
 const request = require('supertest');
 const app = require('../src/app');
+const User = require('../src/user/User');
+const sequelize = require('../src/config/database');
+
+beforeAll(async () => {
+  await sequelize.sync();
+});
+
+beforeEach(() => {
+  return User.destroy({ truncate: true });
+});
+
+const getUsers = async () => {
+  return await request(app).get('/api/v1/users');
+};
+
+const addUsers = async (activeUsercount, inactiveUserCount = 0) => {
+  for (let i = 0; i < activeUsercount + inactiveUserCount; i++) {
+    await User.create({
+      username: `user${i + 1}`,
+      email: `user${i + 1}@mail.com`,
+      inactive: i >= activeUsercount,
+    });
+  }
+};
 
 describe('Listing Users', () => {
   it('returns 200 ok when there are no users in db', async () => {
@@ -15,5 +39,24 @@ describe('Listing Users', () => {
       size: 10,
       totalPages: 0,
     });
+  });
+
+  it('returns 10 users in page content when thre are 11 users in db', async () => {
+    await addUsers(11);
+    const response = await getUsers();
+    expect(response.body.content.length).toBe(10);
+  });
+
+  it('returns 6 users in page content when there are active 6 users and 5 inactive users in db', async () => {
+    await addUsers(6, 5);
+    const response = await getUsers();
+    expect(response.body.content.length).toBe(6);
+  });
+
+  it('returns only id, username and email in content array for each user', async () => {
+    await addUsers(11);
+    const response = await getUsers();
+    const user = response.body.content[0];
+    expect(Object.keys(user)).toEqual(['id', 'username', 'email']);
   });
 });
